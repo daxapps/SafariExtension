883005638b8c2cb7f5da5dbec777b12c43a2c0ba
daxapps <daxapps777@gmail.com>
2017-03-28 09:10:38 -0500

    Add NSExtensionItem

---
+++ Extension/ActionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Jason Crawford on 3/28/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
+            
+            if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
+                
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [unowned self] (dict, error) in
+                    
+                    // do stuff
+                }
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
